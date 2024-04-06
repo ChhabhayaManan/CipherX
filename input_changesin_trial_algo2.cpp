0cfb64325be7b6817ce913a7b3f5c4cefb96cb2c
Kresha2210 <161620844+Kresha2210@users.noreply.github.com>
2024-04-06 17:31:52 +0530

    Update input_changesin_trial_algo2.cpp
    I have added some condition for more efficiency in algorithm.

--- input_changesin_trial_algo2.cpp
+++ input_changesin_trial_algo2.cpp
@@ -26,8 +26,10 @@ void storeData(int memTotal, int slotTotal, int seriesTotal){
             cin >> k;
             if(k > 0 && k <= slotTotal)
                 memVsSlot[i][k-1] = true;
-            else
+            else{
                 cout << "Invalid slot index!" << endl;
+                j--;
+            }
         }
 
         int fav;
@@ -38,8 +40,10 @@ void storeData(int memTotal, int slotTotal, int seriesTotal){
             cin >> k;
             if(k > 0 && k <= seriesTotal)
                 memVsSeries[i][k-1] = true;
-            else
+            else{
                 cout << "Invalid series index!" << endl;
+                j--;
+            }
         }
     }
 }
@@ -57,13 +61,12 @@ void storeSlotVsSeries(int memTotal,int slotTotal, int seriesTotal){
                 }
             }
             slotVsSeries[i][seriesTotal] += slotVsSeries[i][j];
-            slotVsSeries[slotTotal][j] += slotVsSeries[i][j];
         }
     }
 }
 
 void displaySlotVsSeries(int slotTotal, int seriesTotal){
-    for(int i = 0; i < slotTotal+1; i++){
+    for(int i = 0; i < slotTotal; i++){
         for(int j = 0; j < seriesTotal+1; j++){
             cout << slotVsSeries[i][j] << " ";
         }
@@ -74,11 +77,12 @@ void displaySlotVsSeries(int slotTotal, int seriesTotal){
 
 void findMax(int memTotal, int slotTotal, int seriesTotal){
     vector<int> mAX;
+    vector<int>cURR; 
     mAX.push_back(-1);
     mAX.push_back(-1);
     mAX.push_back(-1);
     
-    while(mAX[2] != 0){
+    do{
         mAX.clear();
         mAX.push_back(0);
         mAX.push_back(0);
@@ -108,16 +112,52 @@ void findMax(int memTotal, int slotTotal, int seriesTotal){
                             }
                         } 
                     }
+                    else if(((slotVsSeries[i][seriesTotal] == slotVsSeries[mAX[0]][seriesTotal]) || (i == mAX[0])) ){
+                        for(int k = 0; k < memTotal; k++){
+                            if(memVsSlot[k][i] == true && memVsSeries[k][j] == true){
+                                cURR.push_back(k);
+                            }
+                        } 
+                        int sum = 0,count1=0,count2=0;
+                        for(int k = 0; k < slotTotal; k++){
+                            for(int l = 0; l < cURR.size(); ++l){
+                                if(memVsSlot[cURR[l]][k] == true && memVsSeries[cURR[l]][j] == true){
+                                    count1++;
+                                }
+                            }
+                        }
+                        for(int k = 0; k < slotTotal; k++){
+                            for(int l = 3; l < mAX.size(); ++l){
+                                if(memVsSlot[mAX[l]][k] == true && memVsSeries[mAX[l]][j] == true){
+                                    count2++;
+                                }
+                            }
+                        }
+                        if(count1>count2){
+                            mAX.clear();
+                            mAX.push_back(i);
+                            mAX.push_back(j);
+                            mAX.push_back(slotVsSeries[i][j]);
+                            for(int k = 0; k < memTotal; k++){
+                                if(memVsSlot[k][i] == true && memVsSeries[k][j] == true){
+                                    mAX.push_back(k);
+                                }
+                            } 
+                        }
+                    }
                 }
             }
-        }   
+        }       
+        if(mAX[2]==0)
+        {
+            return;
+        }
         cout << "Slot " << mAX[0]+1 << " Series " << mAX[1]+1 << " : " << endl;
 
         for(int l= 3; l < mAX.size(); l++){
             for(int k = 0; k < slotTotal; k++){
                 if(memVsSlot[mAX[l]][k] == true && memVsSeries[mAX[l]][mAX[1]] == true){
                     slotVsSeries[k][mAX[1]]--;
-                    slotVsSeries[slotTotal][mAX[1]]--;
                 }
             }
             memVsSeries[mAX[l]][mAX[1]] = false;
@@ -127,7 +167,9 @@ void findMax(int memTotal, int slotTotal, int seriesTotal){
         }
         slotVsSeries[mAX[0]] = {0};
         storeSlotVsSeries(memTotal, slotTotal, seriesTotal);
-    }
+        //displaySlotVsSeries(slotTotal, seriesTotal);
+            
+    }while(mAX[2] != 0);
 
 }
 
